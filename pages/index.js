21a7eab695228fc68b82720671100ae1ca1790a1
knwin <49636490+knwins@users.noreply.github.com>
2022-06-25 09:59:06 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
         <title>Rzuki NFT</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Rzuki is a brand" />
+        <meta http-equiv="Access-Control-Allow-Origin" content="*" />
       </Head>
       <Index />
     </div>
